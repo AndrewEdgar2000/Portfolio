82870ef904369741ae6b1a4c3633b2b548317cbc
Andrew Edgar <edgar.andrew@mail.com>
2023-02-16 12:22:22 -0800

    Added animations to cursor index and about.html

--- dist/js/main.js
+++ dist/js/main.js
@@ -30,3 +30,27 @@ function toggleMenu(){
         showMenu = false;
     }
 }
+
+
+
+//Cursor Animation
+
+(() => { 
+
+    const cursor = document.querySelector('.cursor');
+ 
+    document.addEventListener('mousemove', e => {
+       cursor.setAttribute('style', `top:  ${e.pageY - 10}px; left: ${e.pageX -10}px;`);
+    });
+ 
+    document.addEventListener('click', () => { 
+       console.log("%c Click...!!!", "font-size: 20px; color:mediumspringgreen;");
+ 
+       cursor.classList.add('cursor--expand');
+       console.log(cursor.classList);
+    
+       setTimeout(() => {
+          cursor.classList.remove('cursor--expand');
+       }, 500);
+    });
+ })();
