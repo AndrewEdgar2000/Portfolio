e1ba33c470b58444137837abf40fc63b9dc257dc
Andrew Edgar <edgar.andrew@mail.com>
2023-02-23 20:28:55 -0800

    Added contact form functionality

---
+++ dist/php/mail.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = $_POST['name']:
+$email = $_POST['email']:
+$phone = $_POST['phone']:
+$message = $_POST['message']:
+
+$mailHeader = "From: ".$name."<" .$email. ">\r\n":
+
+$recipient = "edgar.andrew@mail.com":
+
+mail($recipient, $message, $mailHeader)
+or die("Error!"):
+
+echo"Message Sent":
+
+
+
+?>
